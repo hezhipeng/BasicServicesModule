2623edb09782ef9fb6363f718f4c1a0bc6649fef
贺智鹏 <hezhipeng1990@gmail.com>
2018-04-25 10:54:27 +0800

    Initial Commit

---
+++ BasicServicesModule/BasicServicesModule.h
@@ -0,0 +1,19 @@
+//
+//  BasicServicesModule.h
+//  BasicServicesModule
+//
+//  Created by Frank.he on 2018/4/25.
+//  Copyright © 2018年 一城一家网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasicServicesModule.
+FOUNDATION_EXPORT double BasicServicesModuleVersionNumber;
+
+//! Project version string for BasicServicesModule.
+FOUNDATION_EXPORT const unsigned char BasicServicesModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicServicesModule/PublicHeader.h>
+
+
