71cc3128c34c4ebba4d1d6070b20919d8fe7d68e
frank.he <frank>
2019-09-23 14:28:26 +0800

    圆角增加边框

--- BasicServicesModule.podspec
+++ BasicServicesModule.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "BasicServicesModule"
-  s.version      = "1.1.3"
+  s.version      = "1.1.4"
   s.summary      = "Basic Services Module"
   s.homepage     = "https://github.com/hezhipeng"
   s.license      = "MIT"
--- BasicServicesModule/Extension/UITableViewCell+Corners.swift
+++ BasicServicesModule/Extension/UITableViewCell+Corners.swift
@@ -18,7 +18,9 @@ extension UITableViewCell {
     ///   - cornerRadius: 角半径
     ///   - indexPath: IndexPath
     ///   - tableView: UITableView
-    public func add(cornerRadius: CGFloat, indexPath: IndexPath, tableView: UITableView) {
+    ///   - borderWidht: borderWidht 默认没有
+    ///   - borderColor: borderColor 默认没有
+    public func add(cornerRadius: CGFloat, indexPath: IndexPath, tableView: UITableView, borderWidth: CGFloat = 0, borderColor: UIColor = .clear) {
         //圆角半径
         let cornerRadius: CGFloat = cornerRadius
         
@@ -27,29 +29,61 @@ extension UITableViewCell {
         let shapeLayer = CAShapeLayer()
         self.layer.mask = nil
         
+        var bPath = UIBezierPath()
+        let ptTopLeft  = CGPoint(x: 0.0, y: 0.0)
+        let ptTopRight = CGPoint(x: bounds.width, y: 0.0)
+        let ptBotRight = CGPoint(x: bounds.width, y: bounds.height)
+        let ptBotLeft  = CGPoint(x: 0.0, y: bounds.height)
+        
         //当前分区有多行数据时
         if sectionCount > 1 {
             switch indexPath.row {
             //如果是第一行,左上、右上角为圆角
             case 0:
-                var bounds = self.bounds
-                bounds.origin.y += 1.0  //这样每一组首行顶部分割线不显示
+                //                var bounds = self.bounds
+                //                bounds.origin.y += 1.0  //这样每一组首行顶部分割线不显示
                 let bezierPath = UIBezierPath(roundedRect: bounds,
                                               byRoundingCorners: [.topLeft,.topRight],
                                               cornerRadii: CGSize(width: cornerRadius,height: cornerRadius))
                 shapeLayer.path = bezierPath.cgPath
-                self.layer.mask = shapeLayer
+                
+                if borderWidth > 0 {
+                    bPath.move(to: ptBotLeft)
+                    bPath.addLine(to: CGPoint(x: ptTopLeft.x, y: ptTopLeft.y + cornerRadius))
+                    
+                    bPath.addArc(withCenter: CGPoint(x: ptTopLeft.x + cornerRadius, y: ptTopLeft.y + cornerRadius), radius: cornerRadius, startAngle: .pi, endAngle: .pi * 1.5, clockwise: true)
+                    
+                    bPath.addLine(to: CGPoint(x: ptTopRight.x - cornerRadius, y: ptTopRight.y))
+                    
+                    bPath.addArc(withCenter: CGPoint(x: ptTopRight.x - cornerRadius, y: ptTopRight.y + cornerRadius), radius: cornerRadius, startAngle: .pi * 1.5, endAngle: 0, clockwise: true)
+                    
+                    bPath.addLine(to: CGPoint(x: ptBotRight.x, y: ptBotRight.y))
+                }
+                
             //如果是最后一行,左下、右下角为圆角
             case sectionCount - 1:
-                var bounds = self.bounds
-                bounds.size.height -= 1.0  //这样每一组尾行底部分割线不显示
+                //                var bounds = self.bounds
+                //                bounds.size.height -= 1.0  //这样每一组尾行底部分割线不显示
                 let bezierPath = UIBezierPath(roundedRect: bounds,
                                               byRoundingCorners: [.bottomLeft,.bottomRight],
                                               cornerRadii: CGSize(width: cornerRadius,height: cornerRadius))
                 shapeLayer.path = bezierPath.cgPath
-                self.layer.mask = shapeLayer
+                
+                if borderWidth > 0 {
+                    bPath = bezierPath
+                }
             default:
-                break
+                var bounds = self.bounds
+                //                bounds.size.height -= 1.0  //这样每一组尾行底部分割线不显示
+                let bezierPath = UIBezierPath.init(rect: bounds)
+                shapeLayer.path = bezierPath.cgPath
+                
+                if borderWidth > 0 {
+                    bPath.move(to: ptBotLeft)
+                    bPath.addLine(to: ptTopLeft)
+                    bPath.addLine(to: ptTopRight)
+                    bPath.addLine(to: ptBotRight)
+                }
             }
         }
             //当前分区只有一行行数据时
@@ -59,7 +93,41 @@ extension UITableViewCell {
                 self.bounds.insetBy(dx: 0.0, dy: 0.0),
                                           cornerRadius: cornerRadius)
             shapeLayer.path = bezierPath.cgPath
-            self.layer.mask = shapeLayer
+            
+            if borderWidth > 0 {
+                bPath = bezierPath
+            }
+        }
+        
+        self.layer.mask = shapeLayer
+        
+        if borderWidth > 0 {
+            if borderLayer == nil {
+                let layer = CAShapeLayer()
+                borderLayer = layer
+                borderLayer?.fillColor = UIColor.clear.cgColor
+                borderLayer?.lineWidth = borderWidth
+                borderLayer?.strokeColor = borderColor.cgColor
+                self.layer.addSublayer(borderLayer!)
+            }
+            borderLayer?.path = bPath.cgPath
+        }
+    }
+    
+    var borderLayer: CAShapeLayer? {
+        get {
+            if let associated = objc_getAssociatedObject(self, &borderLayerKey)
+                as? CAShapeLayer {
+                return associated
+            }
+            return nil
+        }
+        set {
+            objc_setAssociatedObject(self, &borderLayerKey, newValue,
+                                     .OBJC_ASSOCIATION_RETAIN)
         }
     }
 }
+private var borderLayerKey: UInt8 = 23
+
+
